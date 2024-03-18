8d5bb033046aa2570665c214f9a2ed27efd759b7
Shivamj4112 <shivamj4112@gmail.com>
2024-03-18 12:27:19 +0530

    commit

--- app/src/main/java/com/example/stepcounterjetpack/view/Screen/SelectionLoginScreen.kt
+++ app/src/main/java/com/example/stepcounterjetpack/view/Screen/SelectionLoginScreen.kt
@@ -20,11 +20,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
 import com.example.stepcounterjetpack.R
 import com.example.stepcounterjetpack.view.activities.ui.theme.AppColor
 import com.example.stepcounterjetpack.view.activities.ui.theme.BodyTextFont
